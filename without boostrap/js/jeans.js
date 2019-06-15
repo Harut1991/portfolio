b0def2bab6cc6122260774586987b14749846c41
hharutyunyan111@gmail.com <hharutyunyan111@gmail.com>
2019-06-15 19:13:01 +0400

    fixing

--- without boostrap/js/jeans.js
+++ without boostrap/js/jeans.js
@@ -1,22 +1,5 @@
-$(function(){
-	/*$('.li1').click(function(){
-		$('.ul2').toggle();
-	});
-
-	$(document).on("click",".pop-r",function() {
-		$('.left').toggle();
-		$('.header').css('opacity','0.5');
-	})
-	$('.x').click(function(){
-		$('.left').hide()
-	})*/
-	$('.menu').click(function(){
-		$('.jeans-left').toggle();
-
-	})
-
-	//$('.jeans-left').addClass('m-style')
-
-
-
+$(function () {
+    $('.menu').click(function () {
+        $('.jeans-left').toggle();
+    })
 })
