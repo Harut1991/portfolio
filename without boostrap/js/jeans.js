e2c02189bc8b0f554ca4eb42f5849afaab34d5ea
hharutyunyan111@gmail.com <hharutyunyan111@gmail.com>
2019-06-15 18:34:34 +0400

    first commit

---
+++ without boostrap/js/jeans.js
@@ -0,0 +1,22 @@
+$(function(){
+	/*$('.li1').click(function(){
+		$('.ul2').toggle();
+	});
+
+	$(document).on("click",".pop-r",function() {
+		$('.left').toggle();
+		$('.header').css('opacity','0.5');
+	})
+	$('.x').click(function(){
+		$('.left').hide()
+	})*/
+	$('.menu').click(function(){
+		$('.jeans-left').toggle();
+
+	})
+
+	//$('.jeans-left').addClass('m-style')
+
+
+
+})
